ecaf8bea646282f5daf4fe2d00ef0a251e0b9643
gangcheng <gangcheng1030@gmail.com>
2022-08-02 01:49:54 +0800

    account config

--- cmd/superrobot/controller/config/config.go
+++ cmd/superrobot/controller/config/config.go
@@ -33,6 +33,11 @@ var AccountCache = map[string]*model.AccountConfig{
 		Password:    "Cjdmj112233",
 		JunTuanName: "美眉",
 	},
+	"gangcheng11231@163.com": {
+		AccountName: "gangcheng11227@163.com",
+		Password:    "Cjdmj092301",
+		JunTuanName: "熔岩巨兽郭敬明",
+	},
 	"yan7955@126.com": {
 		AccountName: "yan7955@126.com",
 		Password:    "yan7955@",
